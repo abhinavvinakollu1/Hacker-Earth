c062161d4ece1cde47f90ef5827ba4dc4f03aef5
vinakollu v naga sai maruthi abhinav <118501901+abhinavvinakollu1@users.noreply.github.com>
2022-12-06 19:07:48 +0530

    Update Bricks Game.py

--- Bricks Game.py
+++ Bricks Game.py
@@ -4,6 +4,7 @@ Patlu and Motu works in a building construction, they have to put some number of
 They to follow a simple rule, In the i'th round, Patlu puts i bricks whereas Motu puts ix2 bricks.
 
 There are only N bricks, you need to help find the challenge result to find who put the last brick.
+you have to get this time so write the code optimistic way
 
 Input:
 
